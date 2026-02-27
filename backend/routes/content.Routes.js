db8cf484b45faf0f1b6c3cf2780bfec1c96a5407
Your Name <himanshukpr12@gmail.com>
2026-02-27 11:05:55 +0530

    changes in the backend

--- backend/routes/content.Routes.js
+++ backend/routes/content.Routes.js
@@ -2,13 +2,23 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/firebase");
 
-router.get("/home", async (req, res) => {
-  
+router.get("/page", async (req, res) => {
   try {
-    const snapshot = await db.ref("pages/home").once("value");
+    const snapshot = await db.ref("pages/").once("value");
     res.json(snapshot.val());
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+
+router.get("/page/:name", async (req, res) =>{
+  try{
+    const pageName = req.params.name;
+    const snapshot = await db.ref(`pages/${pageName}`).once("value");
+    res.json(snapshot.val());
+  }catch(error){
+    res.status(500).json({error: error.message});
   }
 });
 
